236c81b3556eb08230de3d17d7316efe73c99a53
Viraj1205 <virajshinde1298@gmail.com>
2024-01-25 00:37:46 +0530

    padding fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-
-
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import About from './Components/About';
-import Porfolio from './Components/Portfolio';
-import Experince from './Components/Experience';
-import Contact from './Components/Contact';
-import Sociallinks from './Components/Sociallinks';
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Experience from "./Components/Experience";
+import Home from "./Components/Home";
+import NavBar from "./Components/Navbar";
+import Portfolio from "./Components/Portfolio";
+import SocialLinks from "./Components/Sociallinks";
 
 function App() {
   return (
     <div>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <Porfolio/>
-    <Experince/>
-    <Contact/>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
 
-    <Sociallinks/>
-  </div>
+      <SocialLinks />
+    </div>
   );
 }
 
 export default App;
+
