2dab98d825c2a8185e86d30b43aabf34a27cdab6
Viraj1205 <virajshinde1298@gmail.com>
2024-01-25 22:04:33 +0530

    button

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -65,12 +65,12 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <a target="blank" href={demo}><button  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a target="blank" href={demo} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
-                </button></a>
-                <a target="blank" href={code}><button  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a target="blank" href={code} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
-                </button></a>
+                </a>
               </div>
             </div>
           ))}
