7a52ccc7c7f78c43aed2fce4a8bfbdd7fb55d493
Viraj1205 <virajshinde1298@gmail.com>
2024-02-01 21:01:13 +0530

    Social links

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Experience from "./Components/Experience";
 import Home from "./Components/Home";
 import NavBar from "./Components/Navbar";
 import Portfolio from "./Components/Portfolio";
+import Social from "./Components/Social";
 import SocialLinks from "./Components/Sociallinks";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Social/>
       <Home />
+      
       <About />
       <Portfolio />
       <Experience />
       <Contact />
+
 
       <SocialLinks />
     </div>
---
+++ src/Components/Social.js
@@ -0,0 +1,41 @@
+// Social.js
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="mt-20 py-4 px-2 fixed flex justify-between bg-black w-full text-white lg:hidden md:hidden">
+      <a
+        href="https://www.linkedin.com/in/viraj-shinde-829970222/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-500 px-3 py-2 rounded-full text-white rounded-md hover:scale-110 duration-300"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="mailto:virajshinde1298@gmail.com"
+        className="bg-blue-500 px-3 py-2 rounded-full text-white rounded-md hover:scale-110 duration-300"
+      >
+        Email
+      </a>
+      <a
+        href="https://github.com/Viraj1205"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-500 px-3 py-2 rounded-full text-white rounded-md hover:scale-110 duration-300"
+      >
+        GitHub
+      </a>
+      <a
+        href="./Viraj_s_Resume (2).pdf"
+        target="_blank"
+        download={true}
+        className="bg-blue-500 px-3 py-2 rounded-full text-white rounded-md hover:scale-110 duration-300"
+      >
+        Download Resume
+      </a>
+    </div>
+  );
+};
+
+export default Social;
